d277aed76b5883b32833ac89f0bfcf2f2248b4bc
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2023-04-15 17:28:43 -0500

    uup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,9 +2,14 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import { Box, Card } from "@mui/material"
 
 export default function App() {
   const [count, setCount] = useState(0)
 
-  return <div>HI</div>
+  return (
+    <Box>
+      <Card>hello there</Card>
+    </Box>
+  )
 }
